c3e875c0eda45fb99594711f67463141c9e815bb
Nacho587 <ignaciomacri@outlook.com>
2025-03-27 00:42:38 -0300

    Agregar archivos de proyecto.

---
+++ Models/Users.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembresiaPesca.Models
+{
+    public class Users
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // hace al id un auto incrment en la BdD
+        public int Id { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string AddressFirstLine { get; set; }
+
+        public string AddressSecondLine { get; set; }
+
+        public string AddressCity { get; set; }
+
+        public string PostCode { get; set; }
+
+    }
+}
